610c626b9bfb0df057b7a2c72977d0ed60ca5621
Dominik Wiśniewski <dwisniewski1993@tlen.pl>
2019-11-25 12:55:35 +0100

    Finish result data frame schema

--- SparkScalaTask/src/main/scala/app.scala
+++ SparkScalaTask/src/main/scala/app.scala
@@ -1,13 +1,15 @@
-import org.apache.spark.sql.catalyst.encoders.RowEncoder
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.expressions._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 
 object app extends App {
 
+  // Paths to files
   val us_cities_file: String = "uscitiesv1.5.csv"
   val parking_file: String = "parking-violations-issued-fiscal-year-2014-august-2013-june-2014.csv"
 
+  // Define spark session
   val spark: SparkSession = SparkSession.builder()
     .master("local[*]")
     .appName("Recruitment-Task")
@@ -15,51 +17,39 @@ object app extends App {
 
   import spark.implicits._
 
-  val cities_df: DataFrame = spark.read.format("csv").option("header", "true").load(us_cities_file)
-  val parking_df: DataFrame = spark.read.format("csv").option("header", "true").load(parking_file)
-
-  println(cities_df.columns.toSeq)
-  println(parking_df.columns.toSeq)
-
-  val replacements_state = cities_df.select("state_id", "state_name").rdd
+  // Making mapping for state name and shortcut
+  val replacements_state: scala.collection.Map[String, String] = cities_df.select("state_id", "state_name").rdd
     .map(row => (row.getString(0), row.getString(1))).collectAsMap()
-
-  val schema = StructType(Seq(
-    StructField("State Name", StringType),
-    StructField("Year_Month", StringType),
-    StructField("Ratio", StringType)
-  ))
-
-  val encoder = RowEncoder(schema)
-
-  val population_in_state = cities_df.select("state_id", "population")
+  //Making mapping for population in each state
+  val population_in_state: scala.collection.Map[String, String] = cities_df.select("state_id", "population")
     .na.drop().withColumn("population", $"population".cast("Int"))
     .groupBy("state_id").sum("population").rdd
-    .map(row => (row.getString(0), row.getLong(1).toInt)).collectAsMap()
-
-  val result: DataFrame = parking_df
-    .select("Registration State", "Issue Date")
-    .groupBy($"Registration State".alias("State_Id"), $"Issue Date".alias("Date")).count()
-    .orderBy("Issue Date").map(row => {
-    val row1 = row.getString(0)
-    val row2 = row.getString(1)
-    val row3 = row.getLong(2)
-
-    var state_name: String = null
-    val year_month = row2.split('-').slice(0, 2).mkString("-")
-    var rate: Float = 0.0.toFloat
-
-    if (replacements_state.contains(row1)) {
-      state_name = replacements_state(row1)
-      rate = (row3.toFloat / population_in_state(row1).toFloat)
-    }
-    else {
-      state_name = "Route " + row1
-      rate = 0
-    }
-    Row(state_name, year_month, f"$rate%1.8f")
-  }
-  )(encoder)
+    .map(row => (row.getString(0), row.getLong(1).toString)).collectAsMap()
+  // Load second file to data frame
+  val parking_df: DataFrame = spark.read.format("csv").option("header", "true").load(parking_file)
+  cities_df = null
+  // Defining window partitioner
+  val windowSpec = Window.partitionBy("State Name").orderBy("Year-Month")
+  // Making list with dictionary keys
+  val stateIdsSequence = population_in_state.keys.toList
+  // Load us cities file to data frame
+  var cities_df: DataFrame = spark.read.format("csv").option("header", "true").load(us_cities_file)
+  // Calculate result data frame
+  var result: DataFrame = parking_df
+    .select($"Registration State".as("State Name"), $"Issue Date" as ("Year-Month"))
+    .withColumn("Year-Month", concat_ws("-", slice(split($"Year-Month", "-"), 1, 2)))
+    .groupBy($"State Name", $"Year-Month").count().withColumnRenamed("count", "Ratio")
+    .withColumn("helper", $"State Name")
+    .na.replace("helper", population_in_state.toMap)
+    .withColumn("Ratio", $"Ratio".cast("Float") / $"helper".cast("Float")).drop("helper")
+    .where($"State Name".isin(stateIdsSequence: _*))
+    .na.replace("State Name", replacements_state.toMap)
+    .orderBy(desc("State Name"), asc("Year-Month"))
+    .withColumn("help-100", lit(100))
+    .withColumn("Difference (%)", $"help-100" - when((lag("Ratio", 1)
+      .over(windowSpec)).isNull, null)
+      .otherwise(when($"Ratio" >= lag("Ratio", 1).over(windowSpec), (lag("Ratio", 1).over(windowSpec) * $"help-100") / $"Ratio")
+        .otherwise($"Ratio" * $"help-100") / lag("Ratio", 1).over(windowSpec))).drop("help-100")
 
   result.show()
 
